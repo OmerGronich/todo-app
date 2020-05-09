3c0c1b49096fe81464ea16f061e392c9186cd027
Omer Gronich <gronichomer@gmail.com>
2020-05-09 04:41:11 +0300

    added styling

--- frontend/app.js
+++ frontend/app.js
@@ -24,7 +24,7 @@ addTodoInput.addEventListener('keydown', async e => {
 // Remove todo event
 document.addEventListener('click', e => {
 	if (e.target.id === 'remove-todo') {
-		const todoId = e.target.parentElement.dataset.id;
+		const todoId = e.target.parentElement.parentElement.dataset.id;
 		todosManager.removeTodo(todoId);
 		UI.renderTodoList();
 	}
@@ -33,7 +33,7 @@ document.addEventListener('click', e => {
 // Toggle done status
 document.addEventListener('click', e => {
 	if (e.target.id === 'completed' || e.target.id === 'not-completed') {
-		const todoId = e.target.parentElement.dataset.id;
+		const todoId = e.target.parentElement.parentElement.dataset.id;
 		todosManager.toggleIsDone(todoId);
 		UI.renderTodoList();
 	}
@@ -49,26 +49,40 @@ document.addEventListener('click', async e => {
 
 // Edit todo on dblclick
 document.querySelector('.list').addEventListener('dblclick', e => {
-	if (e.target.dataset.id) {
-		const li = e.target;
+	if (e.target.classList.contains('list-item__content')) {
+		const li = e.target.parentElement.parentElement;
+		li.classList.add('box-shadow-inset');
 		const todos = todosManager.todos;
 		todos.forEach(todo => {
-			if (todo._id === e.target.dataset.id) {
+			if (todo._id === li.dataset.id) {
 				// Is done icon
 				let isDoneIcon = todo.isDone
-					? '<i id="completed" class="far fa-check-circle"></i>'
-					: '<i id="not-completed" class="far fa-circle"></i>';
+					? '<i id="completed" class="far fa-check-circle btn btn--toggle-one"></i>'
+					: '<i id="not-completed" class="far fa-circle btn btn--toggle-one"></i>';
 
 				// Adding input element to html
-				li.innerHTML = `${isDoneIcon} <input type="text" value="${todo.content}" > | <em>${todo.dateCreated}</em> <i id="remove-todo" class="fas fa-times">`;
+				li.innerHTML = `
+					<div class="list-item__left">
+						<span class="hidden">${isDoneIcon}</span>
+						<input class="list-item__input" type="text" value="${todo.content}">
+					</div>`;
+
+				// <div class="list-item__left">
+				// 			${isDoneIcon}
+				// 			<div class="list-item__content">${e.target.value}</div>
+				// 		</div>
+
 				const input = li.querySelector('input');
 				input.focus();
 
 				// Checking for enter key press(recreate submit event)
 				input.addEventListener('keydown', e => {
 					if (e.keyCode === 13) {
-						li.innerHTML = `${isDoneIcon} ${e.target
-							.value} | <em>${todo.dateCreated}</em> <i id="remove-todo" class="fas fa-times">`;
+						li.innerHTML = `<div class="list-item__left">${isDoneIcon} <div class="list-item__content">${e
+							.target
+							.value}</div></div><div class="list-item__right"><em class="list-item__date">${todo.dateCreated}</em><i id="remove-todo" class="fas fa-times btn btn--remove"></div>`;
+
+						li.classList.remove('box-shadow-inset');
 						todosManager.editTodo(todo._id, e.target.value);
 						input.blur();
 					}
@@ -76,11 +90,25 @@ document.querySelector('.list').addEventListener('dblclick', e => {
 
 				// Checking for exiting the focused state
 				input.addEventListener('blur', e => {
-					li.innerHTML = `${isDoneIcon} ${e.target
-						.value} | <em>${todo.dateCreated}</em> <i id="remove-todo" class="fas fa-times">`;
+					li.innerHTML = `
+						<div class="list-item__left">
+							${isDoneIcon} 
+							<div class="list-item__content">${e.target.value}</div>
+						</div>
+						<div class="list-item__right">
+							<em class="list-item__date">${todo.dateCreated}</em>
+							<i id="remove-todo" class="fas fa-times btn btn--remove">
+						</div>`;
+					li.classList.remove('box-shadow-inset');
+
 					todosManager.editTodo(todo._id, e.target.value);
 				});
 			}
 		});
 	}
+});
+
+// Add strikethrough to li content when todos are done
+list.addEventListener('change', () => {
+	UI.checkIfDone();
 });
--- frontend/ui.js
+++ frontend/ui.js
@@ -6,6 +6,18 @@ class UI {
 		list.innerHTML = '';
 	}
 
+	static checkIfDone() {
+		[
+			...list.children
+		].forEach(li => {
+			if (li.querySelector('#completed')) {
+				li.querySelector('.list-item__content').style.textDecoration =
+					'line-through';
+				li.querySelector('.list-item__content').style.opacity = '0.6';
+			}
+		});
+	}
+
 	static async renderTodoList() {
 		UI.clearList();
 
@@ -15,11 +27,20 @@ class UI {
 			todos.forEach(todo => {
 				const todoLiElement = document.createElement('li');
 				todoLiElement.dataset.id = todo._id;
+				todoLiElement.classList.add('list-item');
 				let isDoneIcon = todo.isDone
-					? '<i id="completed" class="far fa-check-circle"></i>'
-					: '<i id="not-completed" class="far fa-circle"></i>';
+					? '<i id="completed" class="far fa-check-circle btn btn--toggle-one"></i>'
+					: '<i id="not-completed" class="far fa-circle btn btn--toggle-one"></i>';
 
-				todoLiElement.innerHTML = `${isDoneIcon} ${todo.content} | <em>${todo.dateCreated}</em> <i id="remove-todo" class="fas fa-times">`;
+				todoLiElement.innerHTML = `
+					<div class="list-item__left">
+						${isDoneIcon}
+						<div class="list-item__content">${todo.content}</div>
+					</div>
+					<div class="list-item__right">
+						<em class="list-item__date">${todo.dateCreated}</em>
+						<i id="remove-todo" class="fas fa-times btn btn--remove">
+					</div>`;
 
 				list.append(todoLiElement);
 				toggleAllBtn.style.display = 'inline-block';
@@ -27,5 +48,7 @@ class UI {
 		} else {
 			toggleAllBtn.style.display = 'none';
 		}
+
+		UI.checkIfDone();
 	}
 }
